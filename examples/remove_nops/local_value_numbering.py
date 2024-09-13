48e8e3632c45fae78100779d917f62263b828b0a
Anirudh <anirudhc@berkeley.edu>
2024-09-13 02:06:13 -0700

    implemented common subexpression elimination in LVN, with commutativity extension

--- examples/remove_nops/local_value_numbering.py
+++ examples/remove_nops/local_value_numbering.py
@@ -11,6 +11,26 @@ class Value():
             final_str += ","
         final_str = final_str[:-1] + ")"
         return final_str
+    def equals(self, val):
+        """
+        Custom equality checker.
+        """
+        if self.op == "const":
+            return False
+        if self.op == val.op:
+            # check if the length of arguments is the same, bc if they arent then vals can't be same
+            # check the length of the set because you want to compare all the unique instances
+            # ex. mul(4,4) vs mul(4,5). It checks if first 4 is in second, passes, then checks second 4 is in sec, passes 
+            # making it into a set will make sure that these duplicates don't show up
+            if len(set(val.args)) != len(set(self.args)):           
+                return False
+            # if len are same, then check that all args are in each other, doesn't matter where you start at this point
+
+            return all([arg in val.args for arg in self.args])
+        
+        return False
+
+
     
 def generate_value(instr, table):
     """
@@ -18,22 +38,32 @@ def generate_value(instr, table):
     Input: Dict{}
     Output: str?
     """
-    print("table: ", table)
     arith_ops = ["add", "sub", "mul", "div"]
     if instr["op"] == "const":
         return Value(op="const", args=[instr["value"]])
     elif instr["op"] in arith_ops:
         consolidated_args = []
-        for arg in instr["args"]:
-            print("arg: ", arg)
-            for num in table:
-                var = table[num][1]
-                if arg == var:
+        for arg in instr["args"]:       # go through all arguments in new instr
+            # print("arg: ", arg)
+            for num in table:           # go through all existing to see if there are overlaps
+                var_list = table[num][1]
+                if arg in var_list:
                     consolidated_args.append(num)
 
         return Value(op=instr["op"], args=consolidated_args)
     else:
         return None
+        # return Value(op=instr["op"], args=[instr["args"]])
+
+def valueInTable(val, tab):
+    """
+    Returns whether the val is in tab. Used to determine is a subexpression has been computed already.
+    """
+    for key in tab.keys():
+        table_value = tab[key][0]
+        if table_value.equals(val):
+            return key
+    return False    # if we make it through all of the keys then we return False
 
 def local_value_numbering(prog):
     """
@@ -47,23 +77,72 @@ def local_value_numbering(prog):
         for instr in fn["instrs"]:
             value = generate_value(instr, table)
             if value:           # if we have generated a value, then we should add it to the table
+                # need to compare with the existing operations and values 
+                val_in_table = valueInTable(value, table)
+                if val_in_table:
+                    table[val_in_table][1].append(instr["dest"])    #table[i] gets the value, [1] gets the first element (variable) which should be a list
+                else:
+                    table[i] = (value, [instr["dest"]])
+                    i += 1
+            # print("table after: ", table)
+    return table
 
-                table[i] = (value, instr["dest"])
-                i += 1
-            
+# TODO: In reflection be aware of the fact that the current implementation only works for ints
+# TODO: Ask about use before instantiation
+def convert_to_instr(instr_value, table):
+    """
+    Converts an instruction that is in deconstructed format into a bril appropriate output
+    """
+    dest = instr_value[1][0]    # 1 is the var name, 0 makes sure that we get only one var name, there may be multiple but we only need one, and for simplicity in the cases that we have only one, it is guaranteed in the 0th index spot
+    val_obj = instr_value[0]    # the value object is stored in the 0th index of the tuple
+    match val_obj.op:
+        case "const":
+            return {'dest': dest, 'op': val_obj.op, 'type': 'int', 'value': val_obj.args[0]}
+        case _:
+            final_simplified = []
+            for arg_val in val_obj.args:
+                final_simplified.append(table[arg_val][1][0])       # we don't want to add the unique number id, we want to add the variable name associated with the argument
+            return {'args': final_simplified, 'dest': dest, 'op': val_obj.op, 'type': 'int'}
             
     
-    return table
+
+def table_to_prog(table):
+    """
+    Converts the table to program format by going through all the keys in our tables, which should, by definition, by unique instructions.
+    """
+    i = 1
+    instruction_list = []
+    while i in table.keys():
+        instr_as_value = table[i]
+        instruction_list.append(convert_to_instr(instr_as_value, table))
+        i += 1
+
+    return instruction_list
 
     
 if __name__ == "__main__":
     # the program comes in from stdin
     prog = json.load(sys.stdin)
+    # print(prog)
+    final_instr = prog["functions"][0]["instrs"][-1]    # very adhoc way of dealing with the final instr, bc curr infra is not able to handle those bc they don't have destinations associated with them
 
+    return_table = local_value_numbering(prog)
+    """
+    table after:  {1: (const(4), ['a']), 2: (const(2), ['b']), 3: (const(2), ['c']), 4: (add(1,2), ['sum1', 'sum2']), 5: (add(1,3), ['sum3']), 6: (mul(4,4), ['prod']), 7: (mul(4,5), ['prod'])}
+    """
+    final_instr_list = table_to_prog(return_table)
+    final_instr_list.append(final_instr)
+    return_prog = {
+        "functions": [
+            {
+                "instrs": final_instr_list,
+                "name" : "main"
+            }
+        ],
+        
+    }
 
-    return_prog = local_value_numbering(prog)
-    print("returned table")
-    print(return_prog)
+    # print(return_prog)
         
     
-    # json.dump(return_prog, sys.stdout, indent=2)
+    json.dump(return_prog, sys.stdout, indent=2)
